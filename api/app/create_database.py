9e6347e41c2dd182374b3d7d371659b865586ef7
Hanif Ali <hanif@protosure.io>
2023-02-06 20:14:48 +0500

    API changes. Add scripts, url normalization

--- api/app/create_database.py
+++ api/app/create_database.py
@@ -2,15 +2,18 @@ import csv
 
 from app.db import SessionLocal, Base, engine
 from app.models import PhishingSite
+from url_normalize import url_normalize
 
 Base.metadata.create_all(bind=engine)
+
+session = SessionLocal()
 
 # Load data from a CSV file
 with open("app/dataset.csv", "r") as file:
     reader = csv.reader(file)
     headers = next(reader)
     for row in reader:
-        data = PhishingSite(url=row[0])
-        SessionLocal.add(data)
+        data = PhishingSite(url=url_normalize(row[0]))
+        session.add(data)
 
-SessionLocal.commit()
+session.commit()
--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, AnyUrl
+from url_normalize import url_normalize
 
 from app.models import PhishingSite
 from app.db import get_db
@@ -13,9 +14,10 @@ class AnalyzeRequestBody(BaseModel):
     url: AnyUrl
 
 
-@app.post("/")
+@app.post("/analyze")
 async def analyze(request_body: AnalyzeRequestBody, db: Session = Depends(get_db)):
-    phishing_site = db.query(PhishingSite).filter_by(url=request_body.url).first()
+    url = url_normalize(request_body.url)
+    phishing_site = db.query(PhishingSite).filter_by(url=url).first()
     if phishing_site:
         return {"phishing": True}
     else:
