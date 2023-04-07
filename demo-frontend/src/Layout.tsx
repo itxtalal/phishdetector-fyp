df8ccaccd2066098cc2b3a6d91188888960a6d7a
Hanif Ali <hanif@protosure.io>
2023-04-07 10:30:38 +0500

    changes

--- demo-frontend/src/Layout.tsx
+++ demo-frontend/src/Layout.tsx
@@ -34,7 +34,7 @@ export default function () {
             </a>
             <a
               className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-              href="https://dashboard.phishdetector.live"
+              href="http://dashboard.phishdetector.live"
             >
               Analytics Report
             </a>
