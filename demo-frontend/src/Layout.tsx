a8176b9412cb01d9a836e2810077344567c1875f
Hanif Ali <hanif@protosure.io>
2023-04-07 11:04:36 +0500

    fix

--- demo-frontend/src/Layout.tsx
+++ demo-frontend/src/Layout.tsx
@@ -21,7 +21,7 @@ export default function () {
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
               className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-              href="/get-extension"
+              href="https://github.com/hanif-ali/phishdetector"
             >
               Get the extension
             </a>
