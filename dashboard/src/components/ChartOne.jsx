070a1de6a7feaa4ac78c4aedd2a58792b3726144
Hanif Ali <hanif@protosure.io>
2023-05-17 13:50:12 +0500

    changes

--- dashboard/src/components/ChartOne.jsx
+++ dashboard/src/components/ChartOne.jsx
@@ -1,6 +1,6 @@
 import ReactApexChart from "react-apexcharts";
 
-import React from "react";
+import React, { useMemo, useState } from "react";
 import ChartThree from "./ChartThree";
 
 const timeFrameDays = {
@@ -9,36 +9,51 @@ const timeFrameDays = {
   year: 12,
 };
 
-const ChartOne = ({ detections, handleTimeFrameChange, timeFrame }) => {
-  const timeFrameDates = Array.from({ length: timeFrameDays[timeFrame] }, (_, i) => {
-    const today = new Date();
+const ChartOne = ({ detections }) => {
+  const [timeFrame, setTimeFrame] = useState("week");
 
-    if (timeFrame == "year") {
-      const date = new Date(today.getFullYear(), today.getMonth() - i, 1);
-      const monthName = date.toLocaleString("default", { month: "long" });
+  const timeFrameDates = Array.from(
+    { length: timeFrameDays[timeFrame] },
+    (_, i) => {
+      const today = new Date();
 
-      return monthName;
+      if (timeFrame == "year") {
+        const date = new Date(today.getFullYear(), today.getMonth() - i, 1);
+        const monthName = date.toLocaleString("default", { month: "long" });
+
+        return monthName;
+      }
+
+      const date = new Date(today);
+      date.setDate(today.getDate() - i);
+
+      const formattedDate = date.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+      });
+      return `${formattedDate}`;
     }
+  ).reverse();
 
-    const date = new Date(today);
-    date.setDate(today.getDate() - i);
-
-    const formattedDate = date.toLocaleDateString("en-US", {
-      month: "short",
-      day: "numeric",
-    });
-    return `${formattedDate}`;
-  }).reverse();
+  const data = useMemo(() => {
+    if (timeFrame == "month") {
+      return {
+        name: "Blck. Listed",
+        data: detections?.last_month || [],
+      };
+    } else {
+      return {
+        name: "Blck. Listed",
+        data: detections?.last_week || [],
+      };
+    }
+  }, [detections]);
 
   const state = {
     series: [
       {
         name: "Blck. Listed",
         data: detections?.last_week || [],
-      },
-      {
-        name: "Wht. Listed",
-        data: [1, 2, 3, 4, 5, 6, 7],
       },
     ],
     options: {
@@ -137,17 +152,17 @@ const ChartOne = ({ detections, handleTimeFrameChange, timeFrame }) => {
           },
         },
         min: 0,
-        max: 100,
+        max: Math.max(...(data.data || [])),
       },
     },
   };
 
   return (
-    <div className="xl:col-span-12/8 col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
+    <div className="xl:col-span-12/8 col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
       <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
         <div className="flex w-full flex-wrap gap-3 sm:gap-5">
           <div className="flex min-w-47.5">
-            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
+            <span className="mr-2 mt-1 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
               <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-primary"></span>
             </span>
             <div className="w-full">
@@ -159,40 +174,6 @@ const ChartOne = ({ detections, handleTimeFrameChange, timeFrame }) => {
                 timeFrameDates[timeFrameDates.length - 1]
               }`}</p>
             </div>
-          </div>
-        </div>
-        <div>
-          <div className="relative z-20 inline-block">
-            <select
-              name="#"
-              id="#"
-              className="relative z-20 inline-flex appearance-none bg-transparent py-1 pl-3 pr-8 text-sm font-medium outline-none"
-              onChange={handleTimeFrameChange}
-            >
-              <option value="week">This Week</option>
-              <option value="month">This Month</option>
-              <option value="year">This Year</option>
-            </select>
-            <span className="absolute top-1/2 right-3 z-10 -translate-y-1/2">
-              <svg
-                width="10"
-                height="6"
-                viewBox="0 0 10 6"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M0.47072 1.08816C0.47072 1.02932 0.500141 0.955772 0.54427 0.911642C0.647241 0.808672 0.809051 0.808672 0.912022 0.896932L4.85431 4.60386C4.92785 4.67741 5.06025 4.67741 5.14851 4.60386L9.09079 0.896932C9.19376 0.793962 9.35557 0.808672 9.45854 0.911642C9.56151 1.01461 9.5468 1.17642 9.44383 1.27939L5.50155 4.98632C5.22206 5.23639 4.78076 5.23639 4.51598 4.98632L0.558981 1.27939C0.50014 1.22055 0.47072 1.16171 0.47072 1.08816Z"
-                  fill="#637381"
-                />
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M1.22659 0.546578L5.00141 4.09604L8.76422 0.557869C9.08459 0.244537 9.54201 0.329403 9.79139 0.578788C10.112 0.899434 10.0277 1.36122 9.77668 1.61224L9.76644 1.62248L5.81552 5.33722C5.36257 5.74249 4.6445 5.7544 4.19352 5.32924C4.19327 5.32901 4.19377 5.32948 4.19352 5.32924L0.225953 1.61241C0.102762 1.48922 -4.20186e-08 1.31674 -3.20269e-08 1.08816C-2.40601e-08 0.905899 0.0780105 0.712197 0.211421 0.578787C0.494701 0.295506 0.935574 0.297138 1.21836 0.539529L1.22659 0.546578ZM4.51598 4.98632C4.78076 5.23639 5.22206 5.23639 5.50155 4.98632L9.44383 1.27939C9.5468 1.17642 9.56151 1.01461 9.45854 0.911642C9.35557 0.808672 9.19376 0.793962 9.09079 0.896932L5.14851 4.60386C5.06025 4.67741 4.92785 4.67741 4.85431 4.60386L0.912022 0.896932C0.809051 0.808672 0.647241 0.808672 0.54427 0.911642C0.500141 0.955772 0.47072 1.02932 0.47072 1.08816C0.47072 1.16171 0.50014 1.22055 0.558981 1.27939L4.51598 4.98632Z"
-                  fill="#637381"
-                />
-              </svg>
-            </span>
           </div>
         </div>
       </div>
@@ -208,7 +189,10 @@ const ChartOne = ({ detections, handleTimeFrameChange, timeFrame }) => {
         </div>
       </div>
       <div className="flex justify-center gap-2">
-        <ChartThree title="Browser Analytics" by_browser={detections?.by_browser} />
+        <ChartThree
+          title="Browser Analytics"
+          by_browser={detections?.by_browser}
+        />
         <ChartThree title="OS Analytics" by_browser={detections?.by_os} />
       </div>
     </div>
